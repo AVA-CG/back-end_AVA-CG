eb290da5bbb953dddbcfcb361518e78ab6e73f73
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-10-31 16:58:04 -0300

    Iniciando o projeto

---
+++ src/main/java/br/com/back_end_AVA_CG/BackEndAvaCgApplication.java
@@ -0,0 +1,13 @@
+package br.com.back_end_AVA_CG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndAvaCgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndAvaCgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/back_end_AVA_CG/BackEndAvaCgApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.back_end_AVA_CG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndAvaCgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
